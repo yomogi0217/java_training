ee6faf87e8a00b509a442743d0feb1090ab5799f
royro <royro@Yomogi-WinPC>
2024-09-13 21:01:30 +0900

    2-修正後

--- 02/jtraining21/jtraining21.java
+++
@@ -1,7 +0,0 @@
-package jtraining21;
-
-public class jtraining21 {		
-	 public static void main(String[] args) {	
-		System.out.println("Hello World!");
-	}	
-}	
---
+++ HelloWorld/src/jtraining22/Jtraining22.java
@@ -0,0 +1,16 @@
+package jtraining22;
+
+/**
+ * Hello World クラス
+ */
+public class Jtraining22 {	
+	/**
+	 * main　メソッド
+	 * param args　実行引数
+	 */
+	 public static void main(String[] args) {
+		String x = "Hello";
+		String y = "World";
+		System.out.println(x + " " + y);
+	}	
+}	
